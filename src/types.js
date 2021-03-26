43f1423070e34c98ee8d3d4626eb934e7649a5a9
frankie nomad <d,hollowind@gmail.com>
2021-03-26 05:39:55 +0000

    first pizzamania-react-app commit

---
+++ src/types.js
@@ -0,0 +1,73 @@
+function Types(){
+    return(
+        <div>
+            <div class="container pizza-types">
+                <div class="row">
+                    <h3 class="text-center">Pizza Types</h3><br/>
+
+                    <div class="col-md-3">
+                        <div class="thumbnail">
+                            <img src="images/ulvi-safari-Ig_BCZ7Ckig-unsplash.jpg" alt="pepperoni"/>
+                            <div class="caption">
+                                <h3>Pepperoni Pizza</h3>
+                                <p>
+                                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                                    Nobis iusto consequuntur soluta nulla maxime optio dolorum
+                                    beatae natus expedita illum magnam voluptatibus, earum sequi
+                                    veritatis, perferendis libero ratione in. Modi!
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="thumbnail">
+                            <img src="images/getulio-moraes-Wz3u9_GtkWc-unsplash.jpg" alt="cheese"/>
+                            <div class="caption">
+                                <h3>Cheese Pizza</h3>
+                                <p>
+                                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                                    Nobis iusto consequuntur soluta nulla maxime optio dolorum
+                                    beatae natus expedita illum magnam voluptatibus, earum sequi
+                                    veritatis, perferendis libero ratione in. Modi!
+                                </p>
+                            </div>
+				        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="thumbnail">
+                            <img src="images/sheri-silver-A4EtgLN1_Fw-unsplash.jpg" alt="vegetable"/>
+                            <div class="caption">
+                                <h3>Vegetable Pizza</h3>
+                                <p>
+                                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                                    Nobis iusto consequuntur soluta nulla maxime optio dolorum
+                                    beatae natus expedita illum magnam voluptatibus, earum sequi
+                                    veritatis, perferendis libero ratione in. Modi!
+                                </p>
+                            </div>
+				        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="thumbnail">
+                            <img src="images/abhishek-k-singh-8z8BsffZ598-unsplash.jpg" alt="bossu"/>
+                            <div class="caption">
+                                <h3>Bossu Pizza</h3>
+                                <p>
+                                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                                    Nobis iusto consequuntur soluta nulla maxime optio dolorum
+                                    beatae natus expedita illum magnam voluptatibus, earum sequi
+                                    veritatis, perferendis libero ratione in. Modi!
+                                </p>
+                            </div>
+				        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Types;
